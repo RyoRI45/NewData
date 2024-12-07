97791ec59abcf105a0c7f4c1da870b96828a7b46
Ryo ICHIHARA <forumgtrgt500@gmail.com>
2024-12-07 22:00:51 +0900

    ナビゲーションバーの追加

--- management/settings.py
+++ management/settings.py
@@ -103,11 +103,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",  # 例えば、プロジェクトの静的ファイルを置く場所
+# ]
 STATICFILES_DIRS = [
-    BASE_DIR / "static",  # 例えば、プロジェクトの静的ファイルを置く場所
+    os.path.join(BASE_DIR, "static"),
 ]
+
 # STATIC_ROOT = '/home/Ryo45/NewData/static'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
