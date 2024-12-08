8d5e39fc4b9bde06f6fe38327dee45e99c00d775
Ryo ICHIHARA <forumgtrgt500@gmail.com>
2024-12-08 16:18:17 +0900

    ナビゲーションバーの改良

--- management/settings.py
+++ management/settings.py
@@ -65,7 +65,6 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.auth',  # 必須
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
