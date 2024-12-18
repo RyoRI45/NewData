0591c49b40d8157f0579ac79bf79a27ca904e53f
Ryo ICHIHARA <forumgtrgt500@gmail.com>
2024-12-19 00:56:33 +0900

    成績画面の改良

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout, authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from .models import Student, Subject, GPA
 from django.http import HttpResponseForbidden
 from core.models import Student  # Student モデルのインポート
